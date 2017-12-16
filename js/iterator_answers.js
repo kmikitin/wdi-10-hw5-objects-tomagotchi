a85a73cdd9396e96c3050fa886f3aecd89086bdc
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-15 21:47:04 -0600

    Commit 10 - reduce done

--- js/iterator_answers.js
+++ js/iterator_answers.js
@@ -19,4 +19,14 @@ const more_words = ['Kait', 'Always', 'Ignores', 'Time'];
 const firstLetter = more_words.map((str) => {
 	return str.charAt(0);
 })
-console.log(firstLetter)
+console.log(firstLetter);
+
+// reduce
+// .reduce iterates over the elements in an array and smooshes them together into a single variable according to the instructions in the iterator. Like .map, it returns a new value and does not alter the original array.
+
+// Return the product of the numbers in arr.
+const arr = [8, 8, 8, 8, 8, 8, 8, 8];
+const product = arr.reduce((a, b) => {
+	return a * b;
+})
+console.log(product);
