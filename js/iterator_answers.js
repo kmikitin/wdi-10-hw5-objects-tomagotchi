59991a55686f64d370abc1963d5b01025efbc515
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-15 21:30:35 -0600

    Commit 8 - forEach done

---
+++ js/iterator_answers.js
@@ -0,0 +1,10 @@
+console.log('WHY IS THE CARPET ALL WET TODD?!?!?');
+
+//forEach 
+// .forEach is a basic iterator; all it does is step through each item in an array.
+// Using the array words, log each word in words in upper case using .forEach.
+const words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey", "Nokor"];
+
+words.forEach((i) => {
+	console.log(i.toUpperCase())
+});
