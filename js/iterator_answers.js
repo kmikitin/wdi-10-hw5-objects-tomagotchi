397a0a6f7583efd9a55b1f19c3fb6fa9e7a38e09
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-15 22:33:57 -0600

    Commit 11 - chaining done

--- js/iterator_answers.js
+++ js/iterator_answers.js
@@ -1,5 +1,8 @@
 console.log('WHY IS THE CARPET ALL WET TODD?!?!?');
 
+
+
+
 //forEach 
 // .forEach is a basic iterator; all it does is step through each item in an array.
 // Using the array words, log each word in words in upper case using .forEach.
@@ -8,6 +11,9 @@ const words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey"
 words.forEach((i) => {
 	console.log(i.toUpperCase())
 });
+
+
+
 
 // Map
 // .map is like .forEach but it returns a new array with the selections and / or transformations applied in the iterator. .map saves to a new array and does not alter the original array.
@@ -21,6 +27,9 @@ const firstLetter = more_words.map((str) => {
 })
 console.log(firstLetter);
 
+
+
+
 // reduce
 // .reduce iterates over the elements in an array and smooshes them together into a single variable according to the instructions in the iterator. Like .map, it returns a new value and does not alter the original array.
 
@@ -30,3 +39,55 @@ const product = arr.reduce((a, b) => {
 	return a * b;
 })
 console.log(product);
+
+
+
+// Chaining
+// Using iterators to find the total sheepCount of sheep shorn by sheep shearers at the 1623 Sheep Shearing Feast:
+const sheepShearers = [
+    {
+        name: "Tim",
+        age: 20,
+        sheepCount: 18
+    },
+    {
+        name: "C3PO",
+        age: 200,
+        sheepCount: 320
+    },
+    {
+        name: "Cousin It",
+        age: Infinity,
+        sheepCount: 2900
+    }
+];
+// Expected Output: => 3238
+
+
+let getSheepCount = Object.keys(sheepShearers).map((i) => {
+	// get just the counts from the object array
+	return sheepShearers[i].sheepCount;
+}).reduce((a, b) => {
+	// add the numbers from the array together
+	return a + b;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
