38b78b844b9645f26a4c6b86b55f82be586fd6e0
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-15 21:41:05 -0600

    Commit 9 - map done

--- js/iterator_answers.js
+++ js/iterator_answers.js
@@ -8,3 +8,15 @@ const words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey"
 words.forEach((i) => {
 	console.log(i.toUpperCase())
 });
+
+// Map
+// .map is like .forEach but it returns a new array with the selections and / or transformations applied in the iterator. .map saves to a new array and does not alter the original array.
+// Map to a new array that stores only the first letter from each word in more_words.
+// const more_words = ["Joe", "Overeats", "Eggs"];
+// Expected result: ["J", "O", "E"]
+
+const more_words = ['Kait', 'Always', 'Ignores', 'Time'];
+const firstLetter = more_words.map((str) => {
+	return str.charAt(0);
+})
+console.log(firstLetter)
