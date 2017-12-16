b24a33fe71a2fa7186bf9022bc927dc4febf972f
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-16 15:58:33 -0600

    Commit 12 - Made a Tamagotchi object

--- js/app.js
+++ js/app.js
@@ -1,168 +1,168 @@
-// console.log('I aim to misbehave.');
+console.log('I aim to misbehave.');
 
-// // Clicker
-// // Make an object called clicker, which has a clickCount property, and a click method.
-// // Have the click method increment the clickCount by 1 each time it is called.
-// let clicker = {
-// 	clickCount: 0,
-// 	click() {
-// 		this.clickCount++;
-// 	}
-// }
-
-
-// // It's All About Me
-// // Create an object called myInformation.
-// // Assign to it your name, your current age, and a fact about you.
-// // Inside myInformation, create a method called statement that returns a statement about you including your name, this fact, and how old you will be in 10 years.
-// // Inside myInformation, create another function that can be used to increment the value of your age by 1 each time that you call it.
-// // It's your birthday! Call this new method that you made that will increase your age.
-// // Create another function that can be used to change the value of your name.
-// // You're feeling funky and decide that your new name is Rumpelstiltskin. Call the method that will update your name and assign this new name. Log your object to confirm the changes.
-// let myInformation = {
-// 	name: 'Kait',
-// 	age: 27,
-// 	hobby: 'running',
-// 	statement() {
-// 		return 'My name is ' + this.name + '. I love ' + this.hobby + ' and I will be ' + (this.age + 10) + ' in 10 years.';
-// 	},
-// 	aging() {
-// 		this.age++;
-// 	},
-// 	newName(newName) {
-// 		this.name = newName;
-// 	}
-// }
-
-// myInformation.aging();
-// myInformation.newName('Rumpelstiltskin');
+// Clicker
+// Make an object called clicker, which has a clickCount property, and a click method.
+// Have the click method increment the clickCount by 1 each time it is called.
+let clicker = {
+	clickCount: 0,
+	click() {
+		this.clickCount++;
+	}
+}
 
 
-// // Greeter
-// // Create an object called greeter.
-// // Inside the greeter object, create a method called hello that takes a name as a parameter and returns a greeting message followed by the name parameter.
-// // Create another method inside greeter called goodbye that takes a name as a parameter and returns a farewell greeting followed by the name parameter.
-// // Create a final method inside greeter called whoAreYou that takes a name as a parameter and returns "Oh right! ", followed by the name parameter, followed by " how could I forget...!"
-// // Play around executing your functions by accessing them through the object.
-// let greeter = {
-// 	hello(otherPerson) {
-// 		return 'Hi there, ' + otherPerson + '!'
-// 	},
-// 	goodbye(otherPerson) {
-// 		return 'Au revoir, ' + otherPerson + ', until we meet again.'
-// 	},
-// 	whoAreYou(otherPerson) {
-// 		return 'Oh right! ' + otherPerson + ' - how could I forget...!'
-// 	}
+// It's All About Me
+// Create an object called myInformation.
+// Assign to it your name, your current age, and a fact about you.
+// Inside myInformation, create a method called statement that returns a statement about you including your name, this fact, and how old you will be in 10 years.
+// Inside myInformation, create another function that can be used to increment the value of your age by 1 each time that you call it.
+// It's your birthday! Call this new method that you made that will increase your age.
+// Create another function that can be used to change the value of your name.
+// You're feeling funky and decide that your new name is Rumpelstiltskin. Call the method that will update your name and assign this new name. Log your object to confirm the changes.
+let myInformation = {
+	name: 'Kait',
+	age: 27,
+	hobby: 'running',
+	statement() {
+		return 'My name is ' + this.name + '. I love ' + this.hobby + ' and I will be ' + (this.age + 10) + ' in 10 years.';
+	},
+	aging() {
+		this.age++;
+	},
+	newName(newName) {
+		this.name = newName;
+	}
+}
 
-// }
-
-// greeter.whoAreYou('Jon'), greeter.hello('Jon');
-// greeter.goodbye('Jon');
-
-// // Collecting Strings
-// // Make an object called stringCollector, which has:
-// // a collection property - an array to hold strings
-// // a collect method which takes a string as a parameter
-// // the string collector prefers the strings to be totally reversed in their collection, however, so make sure to reverse it before adding it to the collection
-// // add an admireCollection method which prints out each string in the collection to the console, one line at a time
-// let stringCollector = {
-// 	collection: [],
-// 	collect(str) {
-// 		let reverseStr = str.split('').reverse().join('');
-// 		this.collection.push(reverseStr);
-// 	},
-// 	admireCollection() {
-// 		for(let i = 0; i <= this.collection.length; i++) {
-// 			console.log(this.collection[i]);
-// 		}
-// 	}
-// }
-
-// stringCollector.collect('whimsical')
-// stringCollector.collect('polenta')
-// stringCollector.collect('exacerbate')
+myInformation.aging();
+myInformation.newName('Rumpelstiltskin');
 
 
-// // ATM
-// // Make an object called atm which has two properties:
-// // totalCash, the total amount of cash in the atm
-// // dispenseTwenties, a method with takes a number of twenties and, if there is enough cash, returns the total number of cash it is giving out.
-// // If there is not enough cash, return null instead.
-// let atm = {
-// 	totalCash: 200,
-// 	dispenseTwenties(num) {
-// 		if (this.totalCash / 20 > 0) {
-// 			this.totalCash = this.totalCash - (num * 20);
-// 			return num * 20;
-// 		} return null;
-// 	}
+// Greeter
+// Create an object called greeter.
+// Inside the greeter object, create a method called hello that takes a name as a parameter and returns a greeting message followed by the name parameter.
+// Create another method inside greeter called goodbye that takes a name as a parameter and returns a farewell greeting followed by the name parameter.
+// Create a final method inside greeter called whoAreYou that takes a name as a parameter and returns "Oh right! ", followed by the name parameter, followed by " how could I forget...!"
+// Play around executing your functions by accessing them through the object.
+let greeter = {
+	hello(otherPerson) {
+		return 'Hi there, ' + otherPerson + '!'
+	},
+	goodbye(otherPerson) {
+		return 'Au revoir, ' + otherPerson + ', until we meet again.'
+	},
+	whoAreYou(otherPerson) {
+		return 'Oh right! ' + otherPerson + ' - how could I forget...!'
+	}
 
-// }
+}
+
+greeter.whoAreYou('Jon'), greeter.hello('Jon');
+greeter.goodbye('Jon');
+
+// Collecting Strings
+// Make an object called stringCollector, which has:
+// a collection property - an array to hold strings
+// a collect method which takes a string as a parameter
+// the string collector prefers the strings to be totally reversed in their collection, however, so make sure to reverse it before adding it to the collection
+// add an admireCollection method which prints out each string in the collection to the console, one line at a time
+let stringCollector = {
+	collection: [],
+	collect(str) {
+		let reverseStr = str.split('').reverse().join('');
+		this.collection.push(reverseStr);
+	},
+	admireCollection() {
+		for(let i = 0; i <= this.collection.length; i++) {
+			console.log(this.collection[i]);
+		}
+	}
+}
+
+stringCollector.collect('whimsical')
+stringCollector.collect('polenta')
+stringCollector.collect('exacerbate')
 
 
-// // Hungry Turtles
-// // Create an object called Leonardo.
-// // Leonardo should have a name attribute of "Leonardo", a color attribute of "Blue", and a weapon attribute of "Katana".
-// // Leonardo should also have a boolean property called pizzaEaten that is initially set to false.
-// // Add a method to Leonardo called eatPizza() which does the following:
-// // If pizzaEaten is true, print out "Leonardo has already eaten pizza" and set pizzaEaten to false.
-// // If pizzaEaten is false, print out "Leonardo is eating pizza" and set pizzaEaten to true.
+// ATM
+// Make an object called atm which has two properties:
+// totalCash, the total amount of cash in the atm
+// dispenseTwenties, a method with takes a number of twenties and, if there is enough cash, returns the total number of cash it is giving out.
+// If there is not enough cash, return null instead.
+let atm = {
+	totalCash: 200,
+	dispenseTwenties(num) {
+		if (this.totalCash / 20 > 0) {
+			this.totalCash = this.totalCash - (num * 20);
+			return num * 20;
+		} return null;
+	}
 
-// let leonardo = {
-// 	name: 'Leonardo',
-// 	color: 'blue',
-// 	weapon: 'Katana',
-// 	pizzaEaten: false,
-// 	eatPizza() {
-// 		if (this.pizzaEaten === false) {
-// 			console.log('Leonardo is eating pizza.');
-// 			this.pizzaEaten = true;
-// 		} else if (this.pizzaEaten === true) {
-// 		console.log('Leonardo has already eaten pizza.');
-// 		}
-// 	}
-// }
-
-// // leonardo.eatPizza()
+}
 
 
-// // Calculator
-// // Create an object called calculator
-// // Create a property called output
-// // Create a method inside your object that will multiply two numbers and change the output value to this current value. This method should also log the value of output.
-// // Create a function that will divide two numbers. It should also change the output value to this current value. This method should also log the value of output.
-// // Practice using your calculator with some pairs (10, 2), (-2, 8), (2, 2).
-// // If there's additional time, add methods for addition and subtraction
-// let calculator = {
-// 	output: 0,
-// 	multiplyTwoNumbers(num1, num2) {
-// 		this.output = num1 * num2;
-// 		console.log(this.output);
-// 	},
-// 	divideTwoNumbers(num1, num2) {
-// 		if (num1 > num2) {
-// 			this.output = num1/num2;
-// 			console.log(this.output);
-// 		} else if (num1 < num2) {
-// 			this.output = num2/num1;
-// 			console.log(this.output);	
-// 		}
-// 	},
-// 	addTwoNumbers(num1, num2) {
-// 		this.output = num1 + num2;
-// 		console.log(this.output);
-// 	},
-// 	subtractTwoNumbers(num1, num2) {
-// 		if (num1 > num2) {
-// 			this.output = num1 - num2;
-// 		console.log(this.output);
-// 		} else if (num1 < num2) {
-// 			this.output = num2 - num1;
-// 			console.log(this.output);
-// 		}
-// 	}
-// }
+// Hungry Turtles
+// Create an object called Leonardo.
+// Leonardo should have a name attribute of "Leonardo", a color attribute of "Blue", and a weapon attribute of "Katana".
+// Leonardo should also have a boolean property called pizzaEaten that is initially set to false.
+// Add a method to Leonardo called eatPizza() which does the following:
+// If pizzaEaten is true, print out "Leonardo has already eaten pizza" and set pizzaEaten to false.
+// If pizzaEaten is false, print out "Leonardo is eating pizza" and set pizzaEaten to true.
+
+let leonardo = {
+	name: 'Leonardo',
+	color: 'blue',
+	weapon: 'Katana',
+	pizzaEaten: false,
+	eatPizza() {
+		if (this.pizzaEaten === false) {
+			console.log('Leonardo is eating pizza.');
+			this.pizzaEaten = true;
+		} else if (this.pizzaEaten === true) {
+		console.log('Leonardo has already eaten pizza.');
+		}
+	}
+}
+
+// leonardo.eatPizza()
+
+
+// Calculator
+// Create an object called calculator
+// Create a property called output
+// Create a method inside your object that will multiply two numbers and change the output value to this current value. This method should also log the value of output.
+// Create a function that will divide two numbers. It should also change the output value to this current value. This method should also log the value of output.
+// Practice using your calculator with some pairs (10, 2), (-2, 8), (2, 2).
+// If there's additional time, add methods for addition and subtraction
+let calculator = {
+	output: 0,
+	multiplyTwoNumbers(num1, num2) {
+		this.output = num1 * num2;
+		console.log(this.output);
+	},
+	divideTwoNumbers(num1, num2) {
+		if (num1 > num2) {
+			this.output = num1/num2;
+			console.log(this.output);
+		} else if (num1 < num2) {
+			this.output = num2/num1;
+			console.log(this.output);	
+		}
+	},
+	addTwoNumbers(num1, num2) {
+		this.output = num1 + num2;
+		console.log(this.output);
+	},
+	subtractTwoNumbers(num1, num2) {
+		if (num1 > num2) {
+			this.output = num1 - num2;
+		console.log(this.output);
+		} else if (num1 < num2) {
+			this.output = num2 - num1;
+			console.log(this.output);
+		}
+	}
+}
 
 
 
---
+++ tamagotchi/tamagotchi.js
@@ -0,0 +1,22 @@
+console.log('We will rule over all this land, and we will call it... "This Land."');
+
+// OBJECT METHODS
+// This weekend you are going to make a tamagotchi object and have it do something. We can store functions inside objects.
+// Start by making a Tamagotchi, just a regular object with properties. Give it the following keys and you can assign whatever values you would like to make it your own: name, creatureType, foodInTummy, restedness, health.
+// Our objects can contain primitive datatypes as values for the keys, and we have seen that they can contain arrays and other objects as values too. Objects can also contain functions as values. When a function is stored within an object, it's called a method.
+// Let's add a method of cry to our Tamagotchi that will log "Waaaaaa!!!" anytime the cry method is called.
+// Invoke the cry method to test that it works.
+
+let tamagotchi = {
+	name: 'Meg',
+	creatureType: 'Sloth',
+	foodInTummy: 5,
+	restedness: 7,
+	health: 10,
+	cry() {
+		console.log('Waaaaaa!!!');
+	}
+}
+
+tamagotchi.cry();
+
