2c3001869786e03f3e102c434e5bdec18b281fbf
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-18 17:03:07 -0600

    Commit 26 - added buttons to run actions on my Tamagotchi, css, and a doc about the game

---
+++ tamagotchi/aboutgame.md
@@ -0,0 +1,14 @@
+# Summary
+This game is a throwback to the little Tamagotchi games made popular in the 90s: https://en.wikipedia.org/wiki/Tamagotchi. I made it for the Web Development Immersive at General Assembly, for practice with objects.
+
+# Game Play
+The game is very basic. Once you hit "start game" a timer begins and the tamagochi needs your help to survive! Users can feed the tamagotchi when it's hungry, put it to sleep when it's sleepy and give it medicine when it's sick. If any of it health points get too low, the tamagotchi dies :( Currently to run it you'll have to download the code onto your own computer.
+
+# Technologies Used
+•HTML
+•CSS
+•JS
+
+# Unsolved Issues
+Adding in the buttons I had to change the game so that you only interact with one of the tamagotchis, though I made two. Later versions would ideally allow you to choose one or play with both. Previous versions of the game also allowed the player to choose what food and medicine they were giving the tamagotchi. I'd also like to add an option for the player to input their own name, with a button that runs the "sayName" method. Most especially I'd love to fix the fact that all three death messages display when the tamagotchi dies.
+
--- tamagotchi/tamagotchi.js
+++ tamagotchi/tamagotchi.js
@@ -1,17 +1,18 @@
 console.log('We will rule over all this land, and we will call it... "This Land."');
 
 // create a function to call the tamagotchi cry method at a set interval
-function hungerTimer(tamagotchi) { 
+function hungerTimer() { 
 	// use set interval to creat a timer that calls cry every 6 seconds
 	const hungerTimer = setInterval(() => {
 	// call the method until there are 0 foods left AS LONG AS the other points haven't run out
-		if (tamagotchi.foodInTummy > 1 && tamagotchi.restedness > 1 && tamagotchi.health > 1) {
-			tamagotchi.cry();
-		} else if (tamagotchi.restedness == 0 || tamagotchi.health == 0) {
-			clearInterval(hungerTime);
+		if (tamagotchi1.foodInTummy > 1 && tamagotchi1.restedness > 1 && tamagotchi1.health > 1) {
+			tamagotchi1.cry();
+			// here I was working on the problem where all three death messages show at once, I want to make it so you just see the one, but the other timers stop.
+		// } else if (tamagotchi.restedness == 0 || tamagotchi.health == 0) {
+		// 	clearInterval(hungerTime);
 		} else {
 			// once there are 0 foods left tamagotchi has starved!
-			console.log(tamagotchi.name + ' starved to death!');
+			console.log(tamagotchi1.name + ' starved to death!');
 			// stop the timer because you killed your tamagotchi with neglect :(
 			clearInterval(hungerTimer);	
 		}
@@ -19,45 +20,45 @@ function hungerTimer(tamagotchi) {
 }
 
 // create a funciton to call the tamagotchi yawn method at a set interval
-function yawnTimer(tamagotchi) { 
+function yawnTimer() { 
 	// use setInterval to call yawn every 10 seconds
 	const yawnTimer = setInterval(() => {
 	// call the method until 0 restedness points remain AS LONG AS the other points haven't run out
-		if (tamagotchi.restedness > 1 && tamagotchi.foodInTummy > 1 && tamagotchi.health > 1) {
-			tamagotchi.yawn();
-		} else if (tamagotchi.foodInTummy == 0 || tamagotchi.health == 0) {
-			clearInterval(yawnTimer);
+		if (tamagotchi1.restedness > 1 && tamagotchi1.foodInTummy > 1 && tamagotchi1.health > 1) {
+			tamagotchi1.yawn();
+			// same as above, working on getting just one death message to show.
+		// } else if (tamagotchi.foodInTummy == 0 || tamagotchi.health == 0) {
+		// 	clearInterval(yawnTimer);
 		} else {
 			// stop the timer and let the player know their tamagotchi died 
-			console.log(tamagotchi.name + ' died of exhaustion!');
+			console.log(tamagotchi1.name + ' died of exhaustion!');
 			clearInterval(yawnTimer);
 		}
 	}, 10000)
 }
 	// create a method to call the tamagotchi yawn method at a set interval 
-function sickTimer(tamagotchi) {
+function sickTimer() {
 		// use setInterval to call puke every 10 seconds
 	const sickTimer = setInterval(() => {
 		// call the puke method until 0 health points remain AS LONG AS the other points haven't run out
-		if (tamagotchi.health > 1 && tamagotchi.foodInTummy > 1 && tamagotchi.restedness > 1) {
-			tamagotchi.puke();
-		} else if (tamagotchi.foodInTummy == 0 || tamagotchi.restedness == 0) {
-			clearInterval(sickTimer);
+		if (tamagotchi1.health > 1 && tamagotchi1.foodInTummy > 1 && tamagotchi1.restedness > 1) {
+			tamagotchi1.puke();
+			// One day only one message will pop up!
+		// } else if (tamagotchi1.foodInTummy == 0 || tamagotchi1.restedness == 0) {
+		// 	clearInterval(sickTimer);
 		} else {
 			// STOP THE TIMER!!! The tamagotchi puked itself to death :/
-			console.log(tamagotchi.name + ' died from their illness!');
+			console.log(tamagotchi1.name + ' died from their illness!');
 			clearInterval(sickTimer);
 		}	
 		}, 25000)
 } 
 
-
-
 // make an object called tamagotchi
-let tamagotchi = {
+let tamagotchi1 = {
 	// give the tamagotchi properties
 	name: 'Meg',
-	creatureType: 'Sloth',
+	creatureType: 'Unicorn',
 	foodInTummy: 10,
 	restedness: 7,
 	health: 5,
@@ -95,7 +96,9 @@ let tamagotchi = {
 		sickTimer(this);
 		yawnTimer(this);
 	},
+	// create a stop method that will clear all interavals
 	stop() {
+		// I changed each property to equal zero because that is the even that stops the intervals from running
 		this.foodInTummy = 0;
 		this.health = 0;
 		this.restedness = 0;
@@ -111,7 +114,7 @@ let tamagotchi = {
 // repeat same as above for a new tamagotchi
 let tamagotchi2 = {
 	name: 'Sid',
-	creatureType: 'Unicorn',
+	creatureType: 'Sloth',
 	foodInTummy: 5,
 	restedness: 5,
 	health: 5,
@@ -161,21 +164,41 @@ let player = {
 		console.log('Hi my name is: ' + this.name)
 	},
 	// create a method for the player to feed both the tamagotchis at once
-	feedTamagotchi (chooseTam, food) {
+	// this code was updated so that you have to choose which tamagotchi you are feeding
+	// Eventually the buttons would have more functionality and you could choose which tamagotchi to fee and what you would feed them
+	// feedTamagotchi (chooseTam, food) {
+	// 	// when they are fed increase their food in tummy by one
+	// 	chooseTam.foodInTummy = chooseTam.foodInTummy + 1;
+	// 	console.log(chooseTam.name + ' just ate a ' + food + '!');
+	// }, 
+	feedTamagotchi () {
 		// when they are fed increase their food in tummy by one
-		chooseTam.foodInTummy = chooseTam.foodInTummy + 1;
-		console.log(chooseTam.name + ' just ate a ' + food + '!');
+		tamagotchi1.foodInTummy = tamagotchi1.foodInTummy + 1;
+		console.log(tamagotchi1.name + ' just ate a rainbow!');
 	}, 
 	// create a method for the player to give medicine to the tamagotchis
-	medicateTamagotchi(chooseTam, medicine) {
+	// this code is to run with two tamagotchis directly in the console (as above)
+	// medicateTamagotchi(chooseTam, medicine) {
+	// 	// when they sleep increase theri health points by 1
+	// 	chooseTam.health = chooseTam.health +1;
+	// 	console.log('You gave ' + chooseTam.name + ' ' + medicine + '.');
+	// },
+	medicateTamagotchi() {
 		// when they sleep increase theri health points by 1
-		chooseTam.health = chooseTam.health +1;
-		console.log('You gave ' + chooseTam.name + ' ' + medicine + '.');
+		tamagotchi1.health = tamagotchi1.health +1;
+		console.log('You gave ' + tamagotchi1.name + ' TUMS.');
 	},
 	// create a method for the player to put the tamagotchis to sleep
-	knockOutTamagotchi(chooseTam){
+	// once again, this was for the two tamagotchi option, pre-buttons
+	// knockOutTamagotchi(chooseTam){
+	// 	// when they sleep increase their restedness points by one
+	// 	chooseTam.restedness = chooseTam.restedness +1;
+	// 	console.log(chooseTam.name + ' is sleeping. Zzzzzzz.')
+	// }
+		knockOutTamagotchi(){
 		// when they sleep increase their restedness points by one
-		chooseTam.restedness = chooseTam.restedness +1;
+		tamagotchi1.restedness = tamagotchi1.restedness +1;
+		console.log(tamagotchi1.name + ' is sleeping. Zzzzzzz.')
 	}
 }
 
@@ -183,6 +206,34 @@ let player = {
 // player.feedTamagotchi()
 
 
+
+
+// add event listeners to each of the buttons
+
+// get the start button
+let startButton = document.getElementById('start');
+// console.log(startButton);
+// have it call the start function
+startButton.addEventListener('click', tamagotchi1.start);
+
+
+// get the feed button
+let feedButton = document.getElementById('feed');
+// have it call the player.feed...method
+feedButton.addEventListener('click', player.feedTamagotchi);
+// console.log(feedButton);
+
+// get the medicate button
+let medicateButton = document.getElementById('medicate');
+// have it call the player.medicate...method
+medicateButton.addEventListener('click', player.medicateTamagotchi);
+// console.log(medicateButton);
+
+// get the rest button
+let knockoutButton = document.getElementById('rest');
+// have it run the player.knockOut...method
+knockoutButton.addEventListener('click', player.knockOutTamagotchi);
+// console.log(knockoutButton);
 
 
 
